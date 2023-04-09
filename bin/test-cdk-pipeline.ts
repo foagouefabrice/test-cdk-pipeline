00cd537953bffc7291dcb1cfecfd92ceda6081e6
Fabrice Foagoue <foagoue_fabrice@bah.com>
2023-04-08 20:31:10 -0400

    update env

--- bin/test-cdk-pipeline.ts
+++ bin/test-cdk-pipeline.ts
@@ -11,7 +11,7 @@ new TestCdkPipelineStack(app, 'TestCdkPipelineStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-   env: { account: '106368237262', region: 'us-east-1' },
+   //env: { account: '106368237262', region: 'us-east-1' },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
